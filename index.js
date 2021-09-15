4f8c23def91f3e6cb56ac9a4ac24b055fcc0628c
stephaniecjohansson <7steph7@gmail.com>
2021-09-14 23:16:01 -0600

    commands

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ initQuestions = () => {
       ])
       .then((response) => {
         Department.create({
-          name: response.departName,
+          name: response.departmentName,
         });
         initQuestions();
       });
@@ -138,7 +138,7 @@ initQuestions = () => {
           type: "list",
           name: "reportingSupervisor",
           message: "Who is your Supervisor?",
-          choices: ["Branch Manager", "Regional Manager", "Original Asst Regional Manager", "Asst Regional Manager", "Regional Director in Charge of Sales"],
+          choices: ["Ed Truck", "Michael Scott", "Dwight K Schrute", "Jim Halpert", "Andy Bernard"],
         },
       ])
       .then((response) => {
@@ -170,5 +170,144 @@ initQuestions = () => {
           : role === "Sales Representative"
           ? (roleId = 11)
         
+        manager === "Ed Truck"
+          ? (managerId = 1)
+          : manager === "Michael Scott"
+          ? (managerId = 2)
+          : manager === "Dwight K Schrute"
+          ? (managerId = 3)
+          : manager === "Jim Halpert"
+          ? (managerId = 4)
+          : manager === "Andy Bernard"
+          ? (managerId = 10)
+          ? (managerId = null)
+          : console.log("ERROR")
   
+        Employee.create({
+          first_name: response.firstName,
+          last_name: response.lastName,
+          role_id: roleId,
+          manager_id: managerId,
+        });
+        initQuestions();
+      });
+  };
+
+
+// Add Rollw/butter
+  addRole = () => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "nameRole",
+          message: "Please enter the name of the role",
+        },
+        {
+          type: "input",
+          name: "roleSalary",
+          message: "What is the salary of the role?",
+        },
+        {
+          type: "list",
+          name: "departmentName",
+          message: "Which department does the role belong to?",
+          choices: ["Management", "Accounting", "Customer Service", "Communications", "Quality Assurance", "Human Resources", "Sales"],
+        },
+      ])
+      .then((response) => {
+        const department = response.departmentName;
+  
+        let departId;
+  
+        department === "Management"
+          ? (departId = 1)
+          : department === "Accounting"
+          ? (departId = 2)
+          : department === "Customer Service"
+          ? (departId = 3)
+          : department === "Communications"
+          ? (departId = 4)
+          : department === "Quality Assurance"
+          ? (departId = 5)
+          : department === "Human Resources"
+          ? (departId = 6)
+          : department === "Sales"
+          ? (departId = 7)
+          : console.log("error");
+  
+        Role.create({
+          title: response.nameRole,
+          salary: response.roleSalary,
+          department_id: departId,
+        });
+        initQuestions();
+      });
+  };
+
+
+// Update Employee Roll
+  async function updateEmp() {
+    const emp = await Employee.findAll();
+    const stringEmp = JSON.stringify(emp);
+    const parseEmp = JSON.parse(stringEmp);
+    let names;
+    workers = () => {
+      parseEmp.forEach((key) => {
+        const firstName = key.first_name;
+        const lastName = key.last_name;
+  
+        names = firstName + " " + lastName;
+        return names;
+      });
+    };
+    workers();
+    console.log(names);
+    const { worker, newrole } = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Choose which employee to update:",
+        name: "employeeUpdate",
+        choices: [names],
+  
+      },
+      {
+        type: "list",
+        message: "What is this employee's new role?",
+        name: "newRole",
+        choices: () => {
+          [" "];
+         
+        },
+      },
+    ]);
+
+
+//Final question
+function finalQuestion() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What would you like to do next?",
+          choices: ["Go back to Start", "Exit"],
+          name: "finalQuestion",
+        },
+      ])
+      .then((answer) => {
+        if (answer.finalQuestion !== "Exit") {
+          //goes back to the beginning
+          init();
+        } else {
+          //clears, logs and ends connection
+          console.clear();
+          logo();
+          console.log("Goodbye!");
+          connection.end();
+        }
+      });
+  }
+  
+  logo();
+  init();
       
